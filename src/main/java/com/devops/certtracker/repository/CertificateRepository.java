1f4e286b0f661bb270820e5b52b6f4ad07ac998c
jato ulrich guiffo kengne <ulrichjato@yahoo.fr>
2023-09-15 11:55:27 -0400

    Added a global Exception Handler for the application

--- src/main/java/com/devops/certtracker/repository/CertificateRepository.java
+++ src/main/java/com/devops/certtracker/repository/CertificateRepository.java
@@ -4,4 +4,5 @@ import com.devops.certtracker.entity.Certificate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CertificateRepository extends JpaRepository<Certificate, Long> {
+
 }
