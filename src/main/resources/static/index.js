5c7d16937f9e08c975e67235d348e02d51d79ef6
Jaewoo Lee <jaewoo.lee90@gmail.com>
2023-09-18 15:34:35 -0400

    Added URL Input Form

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,30 @@
+// handling form submission (fetching POST request)
+const form = document.querySelector('#urlForm');
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const userInput = document.querySelector("#userInputUrl").value;
+
+  addFetch(userInput);
+
+});
+
+// POST request to API for sending URL to the backend
+function addFetch(userInputUrl){
+  let apiUrl = "/api/certificates/add";
+
+  fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': "application/json"
+    },
+    body: JSON.stringify({ url: userInputUrl })
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log('Error:', error)
+    })
+}
