93db02e9cb1a2084fa1028dd94f79fb84a9087e9
jato ulrich guiffo kengne <ulrichjato@yahoo.fr>
2023-09-18 15:18:36 -0400

    Added delete certificate exception handling

--- src/main/java/com/devops/certtracker/exception/EntityNotFoundException.java
+++ src/main/java/com/devops/certtracker/exception/EntityNotFoundException.java
@@ -1,2 +1,14 @@
-package com.devops.certtracker.exception;public class EntityNotFoundException {
+package com.devops.certtracker.exception;
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message){
+        super(message);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public EntityNotFoundException(Throwable cause){
+        super(cause);
+    }
 }
