4590b6a06ff3be6de0750af37374cc713def07e5
jato ulrich guiffo kengne <ulrichjato@yahoo.fr>
2023-11-17 11:19:51 -0500

    Added cors on controllers

--- src/main/java/com/devops/certtracker/controller/PasswordResetController.java
+++ src/main/java/com/devops/certtracker/controller/PasswordResetController.java
@@ -7,9 +7,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @Controller
 @RequiredArgsConstructor
 public class PasswordResetController {
